1439ec17453ac64b1a59c68e886e20302192d1ff
RajankrSingh <52025580+RajankrSingh@users.noreply.github.com>
2022-01-20 15:33:17 +0530

    Create index.js

---
+++ Create_a_Simple_Web_App/index.js
@@ -0,0 +1,19 @@
+import React from 'react';  
+import ReactDOM from  'react-dom';
+
+ReactDOM.render(
+[
+<h1>Welcome to JavaScript Frameworks</h1>,
+<p>Top 5 Frameworks</p>,
+<ol>
+  <li>Angular.js</li>
+  <li>Node.js</li>
+  <li>Vue.js</li>
+  <li>Backbone. js</li>
+  <li>Ember.js</li>
+</ol>
+],
+document.getElementById("root")
+
+);
+
