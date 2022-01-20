b8ed512cbcd2b317d9b25024a063adde871c7714
RajankrSingh <52025580+RajankrSingh@users.noreply.github.com>
2022-01-20 16:10:09 +0530

    Update index.js

--- JavaSccript_Expression_in_JSX_in_React/index.js
+++ JavaSccript_Expression_in_JSX_in_React/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//JavaSccript Expression in JSX in React
+//JavaScript Expression in JSX in React
 
 var f1name = "React";
 let f2name = "Node";
@@ -9,12 +9,12 @@ const f3name = "Ember";
 
 ReactDOM.render(
 [
-<h1>My Name IS {f1name}</h1>, //JavaSccript Expression in JSX in React
+<h1>My Name IS {f1name}</h1>, //JavaScript Expression in JSX in React
 <h1>My Name IS {f2name}</h1>,
 <h1>My Name IS {f3name}</h1>,
 
 <p>My Fav. no is 25</p>,
-<p>My Fav. no is {20+5}</p> //JavaSccript Expression in JSX in React
+<p>My Fav. no is {20+5}</p> //JavaScript Expression in JSX in React
 ],
 
 document.getElementById("root"),
