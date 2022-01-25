7bd5ae3a3b3fef9649659a904f03c87bc5d41019
RajankrSingh <52025580+RajankrSingh@users.noreply.github.com>
2022-01-25 14:31:01 +0530

    Add files via upload

---
+++ Create_Netflix_App_In_React_Js/Sdata.jsx
@@ -0,0 +1,47 @@
+const Sdata = [
+{
+    imgscr : "https://wallpapercave.com/wp/wp4056410.jpg",
+    sname :  "A Netflix Original Series",
+    title:   "DARK",
+    links :  "https://www.netflix.com/in/title/80100172",
+},
+
+{
+    imgscr:"https://wallpapercave.com/wp/wp7418494.jpg",
+    sname:"A Netflix Original Series",
+    title:"Extra curricular",
+    links:"https://www.netflix.com/in/title/80990668",
+},
+
+{
+    imgscr:"https://wallpapercave.com/dwp1x/wp1839578.jpg",
+    sname:"A Netflix Original Series",
+    title:"Stranger things",
+    links:"https://www.netflix.com/in/title/80057281",
+},
+
+{
+    imgscr:"https://m.media-amazon.com/images/M/MV5BZWI4NjY1MmQtN2QwNS00YTQwLWJlNDItMjZjMjE1ODk3MzI4XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+    sname:"A Netflix Original Series",
+    title:"Little Things",
+    links:"https://www.netflix.com/in/title/81011159",
+},
+
+{
+    imgscr:"https://resizing.flixster.com/DdxKzBpxyB6D61amGRNJ0LAOrW0=/ems.ZW1zLXByZC1hc3NldHMvdHZzZXJpZXMvUlRUVjI3OTYxMS53ZWJw",
+    sname:"A Netflix Original Series",
+    title:"Lucifer",
+    links:"https://www.netflix.com/in/title/80057918",
+},
+
+{
+    imgscr:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMS7oahS942ONrJCZjnticmZvLokM74iwjtT3HLamyxsQNsLuU",
+    sname:"A Netflix Original Series",
+    title:"Lost in Space",
+    links:"https://www.netflix.com/in/title/80104198",
+},
+
+
+];
+
+export default Sdata;
