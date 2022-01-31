291af8afff0a33b992365c1cdb4779711b99ea56
RajankrSingh <52025580+RajankrSingh@users.noreply.github.com>
2022-01-31 12:08:08 +0530

    Create index.js

---
+++ UseEffect_Hook/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import UseEffect from './UseEffect';
+
+ReactDOM.render(
+<>
+
+  <UseEffect/>
+
+</>,
+
+document.getElementById('root'));
